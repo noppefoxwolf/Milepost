17bec5b7014ed4418478336d7a87fa77a38c7fd4
noppefoxwolf <noppelabs@gmail.com>
2023-03-19 22:34:11 +0900

    f

--- Sources/bundle-generator/main.swift
+++ Sources/bundle-generator/main.swift
@@ -5,20 +5,22 @@ enum CommandError: LocalizedError {
 }
 
 func run() throws {
+    print(#function, #line)
     let arguments = ProcessInfo().arguments
-
+    print(#function, #line)
     guard arguments.count == 3 else {
         throw CommandError.invalidArguments
     }
-    
+    print(#function, #line)
     let repositoryPath: URL = .init(fileURLWithPath: arguments[1])
     let gitExecutable: URL = .init(fileURLWithPath: "/usr/bin/git")
     let fetcher = Fetcher(repositoryPath: repositoryPath, gitExecutablePath: gitExecutable)
     let revision = try fetcher.parse()
-    
+    print(#function, #line)
     let outputPath = URL(fileURLWithPath: arguments[2])
     let generator = PlistGenerator(outputPath: outputPath)
     try generator.generate(revision)
+    print(#function, #line)
 }
 
 do {
