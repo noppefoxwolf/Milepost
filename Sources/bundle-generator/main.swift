562c1f16fd8fa37b75a9f242a81744ca03c4db7e
noppefoxwolf <noppelabs@gmail.com>
2023-03-19 22:51:21 +0900

    Update main.swift

--- Sources/bundle-generator/main.swift
+++ Sources/bundle-generator/main.swift
@@ -5,22 +5,22 @@ enum CommandError: LocalizedError {
 }
 
 func run() throws {
-    debugPrint(#function, #line)
-    let arguments = ProcessInfo().arguments
-    debugPrint(#function, #line)
-    guard arguments.count == 3 else {
-        throw CommandError.invalidArguments
-    }
-    debugPrint(#function, #line)
-    let repositoryPath: URL = .init(fileURLWithPath: arguments[1])
-    let gitExecutable: URL = .init(fileURLWithPath: "/usr/bin/git")
-    let fetcher = Fetcher(repositoryPath: repositoryPath, gitExecutablePath: gitExecutable)
-    let revision = try fetcher.parse()
-    debugPrint(#function, #line)
-    let outputPath = URL(fileURLWithPath: arguments[2])
-    let generator = PlistGenerator(outputPath: outputPath)
-    try generator.generate(revision)
-    debugPrint(#function, #line)
+    // debugPrint(#function, #line)
+    // let arguments = ProcessInfo().arguments
+    // debugPrint(#function, #line)
+    // guard arguments.count == 3 else {
+    //     throw CommandError.invalidArguments
+    // }
+    // debugPrint(#function, #line)
+    // let repositoryPath: URL = .init(fileURLWithPath: arguments[1])
+    // let gitExecutable: URL = .init(fileURLWithPath: "/usr/bin/git")
+    // let fetcher = Fetcher(repositoryPath: repositoryPath, gitExecutablePath: gitExecutable)
+    // let revision = try fetcher.parse()
+    // debugPrint(#function, #line)
+    // let outputPath = URL(fileURLWithPath: arguments[2])
+    // let generator = PlistGenerator(outputPath: outputPath)
+    // try generator.generate(revision)
+    // debugPrint(#function, #line)
 }
 
 do {
